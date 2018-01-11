da0ad457bb4dea3e0096b47dea1e1e85c54e310d
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-10 20:42:00 -0500

    wrapper div styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import { FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch } from 'react-icons/lib/fa';
 import styled from 'styled-components';
 
+const OuterNavDiv = styled.div`
+  background-color: white;
+  display: flex;
+  /* justify-content: center; */
+  max-width: 1010px;
+  align-items: center;
+  padding: 26px 40px;
+  width: 100%;
+  height: 77px;
+  border-bottom: 1px solid rgba(0,0,0,.0975);
+`
+
+const InnerNavDiv = styled.div`
+display: flex;
+height: 77px;
+justify-content: center;
+max-width: 1010px;
+padding: 26px 40px;
+transition: height .2s ease-in-out;
+width: 100%;
+align-items: center;
+box-sizing: border-box;
+flex-shrink: 0;
+margin: 0;
+position: relative;
+`
+
 class Navbar extends Component{
   render(){
     return (
-      <div>
+      <OuterNavDiv>
+      <InnerNavDiv>
         <h3><FaInstagram /> | Instaclone</h3>
         <FaSearch/><input type="text"/>
         <div>
@@ -13,7 +41,8 @@ class Navbar extends Component{
           <FaHeartO />
           <FaUser />
         </div>
-      </div>
+      </InnerNavDiv>
+      </OuterNavDiv>
     );
   }
 }
